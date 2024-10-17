debbffaebc3827c9e1d2752981aee5b126a630dd
phie <74067483+phie-war-hier@users.noreply.github.com>
2024-10-17 13:23:04 +0200

    pause button

--- timer.js
+++ timer.js
@@ -6,16 +6,16 @@ var seconds = 0;
 
 function setTime() {
     timerFocus = document.getElementById("inputTimerFocus").value;
-    if (timerFocus){
-        document.getElementById("minutes").innerHTML = timerFocus;   
+    if (timerFocus) {
+        document.getElementById("minutes").innerHTML = timerFocus;
     }
-    if(timerFocus > 10) {
-        document.getElementById('nullMin').style.display="none";
+    if (timerFocus > 10) {
+        document.getElementById('nullMin').style.display = "none";
     }
     timerBreak = document.getElementById("inputTimerBreak").value;
-    if (timerBreak){
-        document.getElementById("showBreak").innerHTML = timerBreak;    
-    } 
+    if (timerBreak) {
+        document.getElementById("showBreak").innerHTML = timerBreak;
+    }
 }
 
 var intervalMin;
@@ -34,129 +34,130 @@ var stopSound = new Audio("DigitalClickReversePrimax.mp3");
 
 
 function startSession() {
-    setTime();
-    if(timerFocus==0 && timerBreak==0) {
-        alert("Set your time first");
-    } else {
-        document.getElementById("setTime").style.display = "none";
-        minutes = timerFocus;
-        if(intervalMin === undefined) { 
+    if (intervalMin === undefined) {
+        setTime();
+        if (timerFocus == 0 && timerBreak == 0) {
+            alert("Set your time first");
+        } else {
+            document.getElementById("setTime").style.display = "none";
+            minutes = timerFocus;
+
             startSound.play();
-            document.getElementById('ringStyle').style.borderColor = "var(--tertiary-color)"; 
+            document.getElementById('ringStyle').style.borderColor = "var(--tertiary-color)";
             document.getElementById('message').classList.remove('breakMessage');
             document.getElementById('message').innerHTML = "Session " + counter + " started";
             minutes = minutes - 1;
-            if(minutes > 9) {
-                document.getElementById('nullMin').style.display="none";
+            if (minutes > 9) {
+                document.getElementById('nullMin').style.display = "none";
             }
             document.getElementById('minutes').innerHTML = minutes;
-            
+
             seconds = 59;
-            if(seconds > 9) {
-                document.getElementById('nullSec').style.display="none";
-            } 
+            if (seconds > 9) {
+                document.getElementById('nullSec').style.display = "none";
+            }
             document.getElementById('seconds').innerHTML = seconds;
 
-            intervalMin = setInterval(minutesTimer,60000);
-            intervalSec = setInterval(secondsTimer,1000);
+            intervalMin = setInterval(minutesTimer, 60000);
+            intervalSec = setInterval(secondsTimer, 1000);
 
             function minutesTimer() {
                 minutes = minutes - 1;
                 document.getElementById('minutes').innerHTML = minutes;
-                if(minutes <= 9) {
-                    document.getElementById('nullMin').style.display="block";
+                if (minutes <= 9) {
+                    document.getElementById('nullMin').style.display = "block";
                 } else {
-                    document.getElementById('nullMin').style.display="none";
-                }   
+                    document.getElementById('nullMin').style.display = "none";
+                }
             }
 
             function secondsTimer() {
                 seconds = seconds - 1;
                 document.getElementById('seconds').innerHTML = seconds;
-                if(seconds <= 9) {
-                    document.getElementById('nullSec').style.display="block";
+                if (seconds <= 9) {
+                    document.getElementById('nullSec').style.display = "block";
                 } else {
-                    document.getElementById('nullSec').style.display="none";
+                    document.getElementById('nullSec').style.display = "none";
                 }
-                if(seconds <= 0) {
-                    if(minutes <= 0) {
+                if (seconds <= 0) {
+                    if (minutes <= 0) {
                         minutes = 0;
                         clearInterval(intervalMin);
                         clearInterval(intervalSec);
-                        startBreak();                   
+                        startBreak();
                     }
                     seconds = 60;
                 }
-            } 
-        } else {
-            alert("Timer is already running.");
+            }
         }
+    } else {
+        alert("Timer is already running.");
     }
 }
 
 
 function startBreak() {
     breakSound.play();
-    document.getElementById('se'+counter).style.backgroundColor = "var(--tertiary-color)"; 
-    document.getElementById('ringStyle').style.borderColor = "var(--main-color)"; 
+    document.getElementById('se' + counter).style.backgroundColor = "var(--tertiary-color)";
+    document.getElementById('ringStyle').style.borderColor = "var(--main-color)";
     document.getElementById('message').innerHTML = "Take a break";
     document.getElementById('message').classList.add('breakMessage');
     minutes = timerBreak - 1;
-    if(minutes > 9) {
-        document.getElementById('nullMin').style.display="none";
-    } 
+    if (minutes > 9) {
+        document.getElementById('nullMin').style.display = "none";
+    }
     document.getElementById('minutes').innerHTML = minutes;
     seconds = 59;
-    if(seconds > 9) {
-        document.getElementById('nullSec').style.display="none";
-    } 
+    if (seconds > 9) {
+        document.getElementById('nullSec').style.display = "none";
+    }
     document.getElementById('seconds').innerHTML = seconds;
 
-    intervalMin = setInterval(minutesTimer,60000);
-    intervalSec = setInterval(secondsTimer,1000);
+    intervalMin = setInterval(minutesTimer, 60000);
+    intervalSec = setInterval(secondsTimer, 1000);
 
     function minutesTimer() {
         minutes = minutes - 1;
         document.getElementById('minutes').innerHTML = minutes;
-        if(minutes <= 9) {
-            document.getElementById('nullMin').style.display="block";
+        if (minutes <= 9) {
+            document.getElementById('nullMin').style.display = "block";
         } else {
-            document.getElementById('nullMin').style.display="none";
-        }   
+            document.getElementById('nullMin').style.display = "none";
+        }
     }
 
     function secondsTimer() {
         seconds = seconds - 1;
-        document.getElementById('seconds').innerHTML = seconds;    
-        if(seconds <= 9) {
-            document.getElementById('nullSec').style.display="block";
+        document.getElementById('seconds').innerHTML = seconds;
+        if (seconds <= 9) {
+            document.getElementById('nullSec').style.display = "block";
         } else {
-            document.getElementById('nullSec').style.display="none";
-        }   
-        if(seconds <= 0) {
-            if(minutes <= 0) {
+            document.getElementById('nullSec').style.display = "none";
+        }
+        if (seconds <= 0) {
+            if (minutes <= 0) {
                 minutes = 0;
-                document.getElementById('bre'+counter).style.backgroundColor = "var(--main-color)"; 
+                document.getElementById('bre' + counter).style.backgroundColor = "var(--main-color)";
                 counter = counter + 1;
                 clearInterval(intervalMin);
                 clearInterval(intervalSec);
                 intervalMin = undefined;
-                startSession();                   
+                startSession();
             }
             seconds = 60;
         }
-    }  
+    }
 }
 
 
 
 function resetSession() {
     stopSound.play();
-    for(i=counter; i>0; i--) {
-        document.getElementById('bre'+i).style.backgroundColor = "var(--background-color)"; 
-        document.getElementById('se'+i).style.backgroundColor = "var(--background-color)"; 
-   }
-    document.getElementById('ringStyle').style.borderColor = "var(--main-color)"; 
+    for (i = counter; i > 0; i--) {
+        document.getElementById('bre' + i).style.backgroundColor = "var(--background-color)";
+        document.getElementById('se' + i).style.backgroundColor = "var(--background-color)";
+    }
+    document.getElementById('ringStyle').style.borderColor = "var(--main-color)";
     clearInterval(intervalMin);
     clearInterval(intervalSec);
     minutes = 0;
@@ -166,11 +167,10 @@ function resetSession() {
     document.getElementById('message').classList.remove('breakMessage');
     counter = 1;
     intervalMin = undefined;
-    document.getElementById("setTime").style.display = "flex";     
+    document.getElementById("setTime").style.display = "flex";
 }
 
 
 function pauseSession() {
-    clearInterval(intervalMin);
-            clearInterval(intervalSec);
+    alert("You paused the timer. If you click ok, the timer wil resume.");
 }
